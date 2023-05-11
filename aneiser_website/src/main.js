59291547ed1ab071875931976be76c7305c33a6a
Adrian <a.neiser13@gmail.com>
2023-05-11 18:36:40 +0200

    Install FontAwesome beyond basics included in Vuetify
    1. Add SVG Core
    npm i --save @fortawesome/fontawesome-svg-core
    
    2. Add Icon Packages
    npm i --save @fortawesome/free-solid-svg-icons
    npm i --save @fortawesome/free-regular-svg-icons
    npm i --save @fortawesome/free-brands-svg-icons
    
    3. Add the Vue Component
    # for Vue 3.x
    npm i --save @fortawesome/vue-fontawesome@latest-3

--- aneiser_website/src/main.js
+++ aneiser_website/src/main.js
@@ -10,6 +10,18 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// FontAwesome
+/* the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* specific icons families */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+/* add all icons to the library to use them */
+library.add(fas, far, fab)
+
 const vuetify = createVuetify({
     components,
     directives,
@@ -19,5 +31,6 @@ const app = createApp(App);
 
 app.use(router);
 app.use(vuetify);
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount("#app");
