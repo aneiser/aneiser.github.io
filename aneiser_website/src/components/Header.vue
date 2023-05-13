98286814b51e99a248a8eb3e87c55f14f2502117
Adrian <a.neiser13@gmail.com>
2023-05-13 13:42:54 +0200

    Update header with toolbar component and add custom style

--- aneiser_website/src/components/Header.vue
+++ aneiser_website/src/components/Header.vue
@@ -4,18 +4,53 @@
 
 <template>
     <!-- <v-app-bar elevation="1" scroll-behavior="elevate" scroll-threshold="300" scroll-target="300"> -->
-    <v-app-bar elevation="0" >
+    <v-app-bar elevation="0">
         <v-container>
-            <v-row>
-                <v-btn class="text-subtitle-1">ANEISER</v-btn>
-                <v-spacer></v-spacer>
-                <nav>
-                    <v-btn class="text-subtitle-1"><RouterLink to="/">Home</RouterLink></v-btn>
-                    <v-btn class="text-subtitle-1"><RouterLink to="/about">About</RouterLink></v-btn>
-                    <v-btn class="text-subtitle-1"><RouterLink to="/resume">Resume</RouterLink></v-btn>
-                    <v-btn class="text-subtitle-1"><RouterLink to="/blog">Blog</RouterLink></v-btn>
-                </nav>
-            </v-row>
+            <v-toolbar title="ANEISER" color="white">
+                <v-toolbar-items>
+                  <v-btn class="header__btn" :ripple="false" to="/">Home</v-btn>
+                  <v-btn class="header__btn" :ripple="false" to="/about">About</v-btn>
+                  <v-btn class="header__btn" :ripple="false" to="/resume">Resume</v-btn>
+                  <v-btn class="header__btn" :ripple="false" to="/blog">Blog</v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
         </v-container>
     </v-app-bar>
 </template>
+
+<style>
+    .header__btn {
+        text-transform: none !important;
+    }
+    a.header__btn::before {
+        content: "";
+        position: absolute;
+        top: 73%;
+        left: calc(50% - 5px);
+        border-left: 5px solid transparent;
+        border-right: 5px solid transparent;
+        border-bottom: 8.660254px solid var(--color-black);
+        opacity: 0;
+        transition: opacity 0.2s ease-in-out;
+    }
+
+    a.header__btn.v-btn--active {
+        font-weight: bold !important;
+        color: var(--color-red);
+    }
+
+    a.header__btn.v-btn--active::before,
+    a.header__btn:hover::before,
+    a.header__btn:active::before {
+        opacity: 1;
+    }
+
+    a.header__btn.v-btn--active::before {
+        border-bottom: 8.660254px solid var(--color-red);
+    }
+
+    a.header__btn.v-btn--active > .v-btn__overlay,
+    a.header__btn:hover > .v-btn__overlay {
+        opacity: 0;
+    }
+</style>
