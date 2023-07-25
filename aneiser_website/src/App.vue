85e258132209f3720b3dd65dbce06fd004958a1c
Adrian <a.neiser13@gmail.com>
2023-07-25 15:26:24 +0200

    Create PreFooter.vue component

--- aneiser_website/src/App.vue
+++ aneiser_website/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterView } from "vue-router";
 import Header from "./components/Header.vue";
+import PreFooter from "./components/PreFooter.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
@@ -10,6 +11,7 @@ import Footer from "./components/Footer.vue";
         <v-main>
             <RouterView />
         </v-main>
+        <PreFooter />
         <Footer />
     </v-app>
 </template>
