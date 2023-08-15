0234967aaf48e4218a83c546509ed6a81a042c4a
Adrian <a.neiser13@gmail.com>
2023-08-15 11:31:14 +0200

    Correct and Improve InSignum use case

--- aneiser_website/src/views/Insignum.vue
+++ aneiser_website/src/views/Insignum.vue
@@ -60,31 +60,31 @@ export default {
                                 </div>
                             </div>
                             <ProjectSectionContentText>
-                                <ProjectSectionContentTextP>As you can see in my <a href="../resume.html">Resume</a> I know how to sign language. This is because both of my parents are deaf. As a Child of Deaf Adults (CODA) I grew up seeing the problems and inconveniences of missing sounds implied to them and how they had to made efforts and to develop alternative strategies to be aware of this environmental information that most of us take for granted.</ProjectSectionContentTextP>
-                                <ProjectSectionContentTextP>This led me to work on <a href="https://www.facebook.com/insignum.tech/">InSignum</a>, a personal project (partially framed within academia) that exploit other senses as a substitution for hearing.</ProjectSectionContentTextP>
-                                <ProjectSectionContentTextP>I would love to increases Deaf and hard of Hearing people (DHH) quality of life for real with this project, as such I have been featured in the <a href="https://alumni.eitdigital.eu/article/spotlight-insignum">European Institute of Techonolgy Startup Contest 2020</a>.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>As highlighted in my <a href="../resume.html">resume</a>, I am proficient in sign language. This comes from the fact that both my parents are deaf. Being a Child of Deaf Adults (CODA), I witnessed firsthand the challenges and inconveniences that missing sounds implied to them and how they had to make efforts and to develop alternative strategies to be aware of this environmental information that most of us take for granted.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>This inspired me to initiate <a href="https://www.facebook.com/insignum.tech/">InSignum</a>, a personal project—partially rooted in academia—that leverages alternative senses to compensate for the absence of hearing.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>I am passionate about enhancing the quality of life for Deaf and Hard of Hearing (DHH) individuals through this project. Reflecting this commitment, I was featured in the <a href="https://alumni.eitdigital.eu/article/spotlight-insignum">European Institute of Techonolgy Startup Contest 2020</a>.</ProjectSectionContentTextP>
                             </ProjectSectionContentText>
                         </ProjectSection>
 
                         <ProjectSection>
                             <ProjectSectionContentText>
                                 <ProjectSectionContentTextH3>The problem</ProjectSectionContentTextH3>
-                                <ProjectSectionContentTextP>Environmental sound awareness provides us with meaningful information about our surroundings, moreover, hearing is often the exclusive sense for the detection of many everyday life events. However, Deaf and Hard of Hearing (DHH) people have the impossibility, or at least the challenge, to perceive these informative signals decreasing their quality of life consequently.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>Environmental sound awareness provides us with meaningful information about our surroundings, moreover, hearing is often the exclusive sense for the detection of many everyday life events. However, for Deaf and Hard of Hearing (DHH) individuals, perceiving these signals can be impossible or challenging, subsequently affecting their quality of life.</ProjectSectionContentTextP>
                             </ProjectSectionContentText>
                         </ProjectSection>
 
                         <ProjectSection>
                             <ProjectSectionContentText>
                                 <ProjectSectionContentTextH3>The process</ProjectSectionContentTextH3>
-                                <ProjectSectionContentTextP>Although I know first-hand the problem, sign language and the deaf culture and its idiosyncrasy I did not neglect the fundamental aspects of User Centered Design Being a proxy user helped me to empathize with users and being more conscious and closer to the problems they encounter and their needs.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>While I have firsthand knowledge of the challenges, sign language, and deaf culture and its idiosyncrasies, I did not neglect the fundamental aspects of User-Centered Design. Being a proxy user helped me to empathize with users and being more conscious and closer to the problems they encounter and their needs.</ProjectSectionContentTextP>
 
                                 <ProjectSectionContentTextH4>User research</ProjectSectionContentTextH4>
-                                <ProjectSectionContentTextP>Semi-structured interviews regarding their problems and needs did not add much more information that the one I already had; however, it was a good to confirm insights and discover some others.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>While semi-structured interviews largely reaffirmed what I already knew, they did help validate certain insights and unveil a few new ones.</ProjectSectionContentTextP>
                                 <ProjectSectionContentTextP>Throughout my life I have seen multitude of occasions where my parents were missing sound awareness from life-threatening events such an approaching car honking to everyday convenience like knowing when the washing machine is beeping to know when to take out the clothes.</ProjectSectionContentTextP>
                                 <ProjectSectionContentTextP>Preferred modalities, contexts of use and social issues arose during this stage.</ProjectSectionContentTextP>
 
                                 <ProjectSectionContentTextH4>Exploring some approaches with design artifacts</ProjectSectionContentTextH4>
-                                <ProjectSectionContentTextP>For the different problems (missing sound warnings, accessibility, and stigmatization...) and need founds several potential solutions (light emitting objects, or light embedded on walls or floors...) came from the brainstorming sessions.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>The brainstorming sessions generated potential solutions (like light-emitting objects or embedded lighting in walls or floors) to address a range of issues, including missed sound alerts, accessibility challenges, and stigmatization.</ProjectSectionContentTextP>
                             </ProjectSectionContentText>
 
                             <ProjectSectionContentImage>
@@ -94,7 +94,7 @@ export default {
 
                             <ProjectSectionContentText>
                                 <ProjectSectionContentTextH5>Smartlight system</ProjectSectionContentTextH5>
-                                <ProjectSectionContentTextP>Upgrading already familiar lightbulb uses with newer technology such multi-coloured LED and, Internet of Things (IoT) capabilities creating a kind of a smarts home was one potential solution.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>Upgrading already familiar lightbulb uses with newer technology such multi-coloured LED and, Internet of Things (IoT) capabilities to create a kind of smart home was one potential solution.</ProjectSectionContentTextP>
                                 <ProjectSectionContentTextP>To introduce the concept and elicit reactions some teammates and I, developed a hardware and software prototype of a house equipped with RGB LEDs which illuminate the path towards the sound source indicating the nature of the sound via the colour of the lights. The following video showcases the concept of how would a deaf person interact with the system.</ProjectSectionContentTextP>
                                 <ProjectSectionContentTextP>We also added some control features using a smartwatch to fulfil some needs and test ideas from our brainstorming sessions.</ProjectSectionContentTextP>
                             </ProjectSectionContentText>
@@ -110,8 +110,8 @@ export default {
                             <ProjectSectionContentText>
                                 <ProjectSectionContentTextH5>Wearable</ProjectSectionContentTextH5>
                                 <ProjectSectionContentTextP>Touch sense was the option to go to offer a discreet and outdoors sound awareness solution.</ProjectSectionContentTextP>
-                                <ProjectSectionContentTextP>Develop a prototype for a wearable device is costly. To keep it simple we decided just to represent the concept in a video prototype showing how a vibrating wearable would work.</ProjectSectionContentTextP>
-                                <ProjectSectionContentTextP>At the beginning, it shows problematic situations for deaf people that are later repeated, this time, showing how they would look like using the wearable. You can also see me hands on the wearable prototype</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>Developing a prototype for a wearable device can be costly. To simplify, we opted to demonstrate the concept using a video prototype that illustrates the functionality of a vibrating wearable.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>At the beginning, it shows problematic situations for deaf people that are later repeated, this time, showing how they would look like using the wearable. You can also see me creating the wearable prototype.</ProjectSectionContentTextP>
                             </ProjectSectionContentText>
 
                             <ProjectSectionContentVideo>
@@ -124,7 +124,7 @@ export default {
 
                             <ProjectSectionContentText>
                                 <ProjectSectionContentTextP>Although both concepts were well received the "all-in-one" nature of a wearable was preferred as it would serve for both, indoors and outdoors contexts.</ProjectSectionContentTextP>
-                                <ProjectSectionContentTextP>I continued solo with the project from this moment on. I delved into the wearables field. Research in this aspect throw multitude of form factors, requirements depending on contexts and social considerations.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>From this point, I pursued the project independently. I delved into the wearables field. Research in this domain revealed a multitude of form factors, requirements depending on contexts and social considerations.</ProjectSectionContentTextP>
                                 <ProjectSectionContentTextP>In parallel with this state-of-the-art research, I did more surveys, focus groups and, interviews as part of user research and User Centered Design.</ProjectSectionContentTextP>
                             </ProjectSectionContentText>
 
@@ -134,8 +134,8 @@ export default {
                             </ProjectSectionContentImage>
 
                             <ProjectSectionContentText>
-                                <ProjectSectionContentTextP>As in any iterative process there are many outcomes and loops within. However, this section shows the last outcome from the project.</ProjectSectionContentTextP>
-                                <ProjectSectionContentTextP>One of the most common answers when I asked for opinion for wearables was that without trying, they could not do more than guess. Being conscious about that imagining and experiment are two different things I started to test fast if vibration was suitable feedback and if mapping loudness with vibration intensity was adequate.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>As with any iterative process, various outcomes and iterations emerged. However, this section shows the last outcome from the project.</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>One of the most common answers when I asked for opinion for wearables was that without trying, they could not do more than guess. Recognizing that visualizing and experiencing are distinct, I conducted preliminary, rapid tests to determine if vibration was a suitable form of feedback and if mapping loudness with vibration intensity was adequate.</ProjectSectionContentTextP>
                             </ProjectSectionContentText>
                         </ProjectSection>
 
@@ -150,7 +150,7 @@ export default {
                             </ProjectSectionContentImage>
                             <ProjectSectionContentText>
                                 <ProjectSectionContentTextP>The results of the experiments indicate that the solution has potential, and I am actually working further on smartlight systems and wearable systems that provide deaf people with the perception of sound in everyday situations through haptic and sight feedback to make the life of Deaf and Hard of Hearing people easier.</ProjectSectionContentTextP>
-                                <ProjectSectionContentTextP>If you want to know more follow us on <a href="https://www.facebook.com/insignum.tech" target="_blank">Facebook</a> or <a href="https://www.instagram.com/insignumtech/" target="_blank">Instagram</a>. If you want to participate in the project feel free to <a href="../contact.html">contact me</a>, I will be glad to hear from you!</ProjectSectionContentTextP>
+                                <ProjectSectionContentTextP>Your support and engagement can make a difference in this venture. If you're intrigued and wish to know more, follow us on <a href="https://www.facebook.com/insignum.tech" target="_blank">Facebook</a> or <a href="https://www.instagram.com/insignumtech/" target="_blank">Instagram</a>. For those keen on contributing or participating in the project, don't hesitate to <a href="../contact.html">reach out</a>. I'd love to hear your insights and ideas!</ProjectSectionContentTextP>
                             </ProjectSectionContentText>
                         </ProjectSection>
 
