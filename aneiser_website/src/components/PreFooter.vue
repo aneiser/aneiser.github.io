d12d3215b4283475fe69a8981a6e356a64615fd0
Adrian <a.neiser13@gmail.com>
2023-08-28 12:16:21 +0200

    Add a red bar in the pre-footer to make a background for the title

--- aneiser_website/src/components/PreFooter.vue
+++ aneiser_website/src/components/PreFooter.vue
@@ -2,31 +2,32 @@
 </script>
 
 <template>
+    <section class="bg--red-dark"><v-container></v-container></section>
     <section class="bg--red-darker">
-            <v-container>
-                <h3 class="text-h4 section__title">Let's get in touch</h3>
-                <div class="section">
-                    <v-row>
-                        <v-col class="text-center">
-                            <h4 class="text-h5 section__subtitle">Wow! All the way down, that shows dedication!</h4>
-                            <p class="text-body-1">Thank you for taking the time to review my work.</p>
+        <v-container>
+            <h3 class="text-h4 section__title">Let's get in touch</h3>
+            <div class="section">
+                <v-row>
+                    <v-col class="text-center">
+                        <h4 class="text-h5 section__subtitle">Wow! All the way down, that shows dedication!</h4>
+                        <p class="text-body-1">Thank you for taking the time to review my work.</p>
                         <p class="text-body-1">Explore potential collaborations by <a href="https://www.linkedin.com/in/aneiser-ux/" target="_blank" class="bg--red-darker">connecting with me on LinkedIn</a>.</p>
-                            <p class="d-flex justify-center footer__icons">
-                                <a href="https://github.com/aneiser">
-                                    <font-awesome-icon icon="fab fa-github"></font-awesome-icon>
-                                </a>
+                        <p class="d-flex justify-center footer__icons">
+                            <a href="https://github.com/aneiser">
+                                <font-awesome-icon icon="fab fa-github"></font-awesome-icon>
+                            </a>
                             <a href="https://www.linkedin.com/in/aneiser-ux/" target="_blank">
-                                    <font-awesome-icon icon="fab fa-linkedin"></font-awesome-icon>
-                                </a>
-                                <!-- <a href="">
-                                <font-awesome-icon icon="fab fa-medium"></font-awesome-icon>
-                            </a> -->
-                            </p>
-                            <p class="text-body-1">If you'd like to talk more, share feedback or ask anything, feel free to reach me out at <a href="mailto:a.neiser13@gmail.com" class="bg--red-darker" target="_blank">hello@aneiser.com</a></p>
-                            <p class="text-body-1">I'd love to hear your thoughts!</p>
-                        </v-col>
-                    </v-row>
-                </div>
-            </v-container>
-        </section>
+                                <font-awesome-icon icon="fab fa-linkedin"></font-awesome-icon>
+                            </a>
+                            <!-- <a href="">
+                            <font-awesome-icon icon="fab fa-medium"></font-awesome-icon>
+                        </a> -->
+                        </p>
+                        <p class="text-body-1">If you'd like to talk more, share feedback or ask anything, feel free to reach me out at <a href="mailto:a.neiser13@gmail.com" class="bg--red-darker" target="_blank">hello@aneiser.com</a></p>
+                        <p class="text-body-1">I'd love to hear your thoughts!</p>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
+    </section>
 </template>
