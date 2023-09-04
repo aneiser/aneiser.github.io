6ab59d692e82d3b13441f6aa20c83b2a10fdd726
Adrian <a.neiser13@gmail.com>
2023-09-04 08:59:47 +0200

    Update footer text

--- aneiser_website/src/components/Footer.vue
+++ aneiser_website/src/components/Footer.vue
@@ -4,7 +4,7 @@
 <template>
     <v-footer class="d-flex flex-column text-center bg--red-darker">
         <div class="text-caption">
-            Crafting human-centered designs with a diversity touch. Currently based in Paris, fueled by food and daydreaming vs dreams.
+            Crafting human-centered and inclusive designs from Paris with love. Inspired by culinary delights and daydreams.
         </div>
         <div class="text-caption">
             Website design and content Copyright &copy; 2016 - {{ new Date().getFullYear() }} Adrián Neila Serrano.
