e293c76d0d7809e95ae8bec5ab837f0ce17635cd
Adrian <a.neiser13@gmail.com>
2023-09-04 08:51:50 +0200

    Create CV / Resume page

--- aneiser_website/src/views/Resume.vue
+++ aneiser_website/src/views/Resume.vue
@@ -2,6 +2,17 @@
 </script>
 
 <template>
-    <h1>Resume</h1>
-    <p>Page content goes here.</p>
+    <article class="bg--red-dark">
+        <iframe src="/Adrian · UX Designer.pdf#page=1&view=Fit" class="resume-pdf">
+            It seems like your browser doesn't support viewing PDFs. You can <a href="/Adrian · UX Designer.pdf" target="_blank">download the resume here</a>.
+        </iframe>
+    </article>
 </template>
+
+<style scoped>
+    .resume-pdf {
+        width: 100%;
+        height: calc(100vh - 64px);
+        border: none;
+    }
+</style>
