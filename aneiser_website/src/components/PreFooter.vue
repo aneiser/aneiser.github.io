dfb81ff9d81e3355c054a82ff7b0a30227a54ce9
Adrian <a.neiser13@gmail.com>
2023-09-04 09:01:34 +0200

    Temporally hide GitHub and Medium icons in the footler

--- aneiser_website/src/components/PreFooter.vue
+++ aneiser_website/src/components/PreFooter.vue
@@ -13,15 +13,15 @@
                         <p class="text-body-1">Thank you for taking the time to review my work.</p>
                         <p class="text-body-1">Explore potential collaborations by <a href="https://www.linkedin.com/in/aneiser-ux/" target="_blank" class="bg--red-darker">connecting with me on LinkedIn</a>.</p>
                         <p class="d-flex justify-center footer__icons">
-                            <a href="https://github.com/aneiser">
+                            <!-- <a href="">
                                 <font-awesome-icon icon="fab fa-github"></font-awesome-icon>
-                            </a>
+                            </a> -->
                             <a href="https://www.linkedin.com/in/aneiser-ux/" target="_blank">
                                 <font-awesome-icon icon="fab fa-linkedin"></font-awesome-icon>
                             </a>
                             <!-- <a href="">
-                            <font-awesome-icon icon="fab fa-medium"></font-awesome-icon>
-                        </a> -->
+                                <font-awesome-icon icon="fab fa-medium"></font-awesome-icon>
+                            </a> -->
                         </p>
                         <p class="text-body-1">If you'd like to talk more, share feedback or ask anything, feel free to reach me out at <a href="mailto:a.neiser13@gmail.com" class="bg--red-darker" target="_blank">a.neiser13@gmail.com</a></p>
                         <p class="text-body-1">I'd love to hear your thoughts!</p>
