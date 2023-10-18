cd503a14752a1e2dbceb0bf69b936bbb7af2aee1
Adrian <a.neiser13@gmail.com>
2023-10-18 17:59:22 +0200

    Refactoring: Move projects views to projects folder

--- aneiser_website/src/router/index.js
+++ aneiser_website/src/router/index.js
@@ -9,18 +9,19 @@ import Home from "../views/Home.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/",       name: "home",   component: Home },
+    { path: "/",                      name: "home",         component: Home },
     // ...Use "route level code-splitting" to optimize the loading and rendering.
     // This can significantly reduce the initial load time and improve the overall performance.
-    { path: "/about", name: "about", component: () => import("../views/About.vue") },
-    { path: "/resume", name: "resume", component: () => import("../views/Resume.vue") },
-    { path: "/a4-resume", name: "A4Resume", component: () => import("../views/A4_resume.vue") },
-    { path: "/blog", name: "blog", component: () => import("../views/Blog.vue") },
-    { path: "/projects/ubisoft", name: "ubisoft", component: () => import("../views/Ubisoft.vue") },
-    { path: "/projects/smartcontact", name: "smartcontact", component: () => import("../views/Smartcontact.vue") },
-    { path: "/projects/insignum", name: "insignum", component: () => import("../views/Insignum.vue") },
-    { path: "/projects/hera", name: "hera", component: () => import("../views/Hera.vue") },
-    { path: "/projects/el-mundo", name: "el-mundo", component: () => import("../views/Elmundo.vue") },
+    { path: "/about",                 name: "about",        component: () => import("../views/About.vue") },
+    { path: "/resume",                name: "resume",       component: () => import("../views/Resume.vue") },
+    { path: "/a4-resume",             name: "A4Resume",     component: () => import("../views/A4_resume.vue") },
+    { path: "/blog",                  name: "blog",         component: () => import("../views/Blog.vue") },
+    // projects
+    { path: "/projects/el-mundo",     name: "el-mundo",     component: () => import("../views/projects/Elmundo.vue") },
+    { path: "/projects/hera",         name: "hera",         component: () => import("../views/projects/Hera.vue") },
+    { path: "/projects/insignum",     name: "insignum",     component: () => import("../views/projects/Insignum.vue") },
+    { path: "/projects/smartcontact", name: "smartcontact", component: () => import("../views/projects/Smartcontact.vue") },
+    { path: "/projects/ubisoft",      name: "ubisoft",      component: () => import("../views/projects/Ubisoft.vue") },
   ],
 });
 
