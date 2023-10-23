7e3f37d984b5ba530beef594486308d09e87417f
Adrian <a.neiser13@gmail.com>
2023-10-23 13:18:37 +0200

    Update /dist

--- assets/About-a42fc621.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o,c as n,F as c,a as e}from"./index-688c4477.js";const s={},a=e("h1",null,"About",-1),r=e("p",null,"Page content goes here.",-1);function _(l,u){return o(),n(c,null,[a,r],64)}const f=t(s,[["render",_]]);export{f as default};
--- assets/Resume-ef1e08c8.js
+++
@@ -1,3 +0,0 @@
-import{_ as s,o as c,c as o,p as t,d as a,b as r}from"./index-25cc7d2a.js";const _={},d=e=>(t("data-v-cf8efec5"),e=e(),a(),e),n={class:"bg--red-dark"},p=d(()=>r("iframe",{src:"/Adrian · UX Designer.pdf#page=1&view=Fit",class:"resume-pdf"},`\r
-            It seems like your browser doesn't support viewing PDFs. You can <a href="/Adrian · UX Designer.pdf" target="_blank">download the resume here</a>.\r
-        `,-1)),i=[p];function f(e,u){return c(),o("article",n,i)}const m=s(_,[["render",f],["__scopeId","data-v-cf8efec5"]]);export{m as default};
